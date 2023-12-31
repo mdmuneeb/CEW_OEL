7ce021f064ffe37c314ebab07ec47c4ee0db402e
mdmuneeb <muneebzafar999@gmail.com>
2023-12-31 10:09:23 -0800

    Write a api data to file

--- OEL.c
+++ OEL.c
@@ -11,9 +11,22 @@ int main() {
 
     char url[] = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=portugal&appid=7821014de233ecf098180185706f59ca";
 
+    FILE *file;
+    
+
     if (curl) {
         // Set the URL
         curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        file = fopen("Weather_Data.txt", "w");
+
+        if (file == NULL)
+        {
+            printf("There is some error");
+        }
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
 
         // Perform the request
         result = curl_easy_perform(curl);
@@ -22,9 +35,14 @@ int main() {
         if (result != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(result));
 
+ 
         // Cleanup
         curl_easy_cleanup(curl);
+
+        fclose(file);
     }
+
+
 
     // Cleanup libcurl
     curl_global_cleanup();
